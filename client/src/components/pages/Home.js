9fc2d8b7a31ccc569d33c07f74b8ea9bacd9d20c
nodokaodayaka <wankosob@gmail.com>
2020-12-04 20:03:50 +0900

    day3

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
-  return <div>Home Component....</div>;
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      axios.get("http://localhost:4000").then((result) => {
+        setList(result.data);
+      });
+    }, 0);
+  }, []);
+
+  console.log(list.length);
+  if (list.length < 1) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <ul>
+      {list.map((v, k) => (
+        <li key={k}>
+          <h2>{v.title}</h2>
+          <p>{v.description}</p>
+          <img src={v.imageUrl} alt="image" />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Home;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,32 @@
 const express = require("express");
+const cors = require('cors')
 
-const app = express();
 const port = 4000;
+const app = express();
+app.use(cors())
 
-app.get("/", (req, res) => { res.send("Hello world") });
-app.get("/test", (req, res) => { res.send("Test Ready") });
-app.listen(port, () => { console.log("http://localhost:4000 start") });
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  res.json([
+    {title:"title 1",
+    description:'description 1',
+    imageUrl:'http://localhost:4000/image/bob1.jpg'},
+    {title:"title 2",
+    description:'description 2',
+    imageUrl:'http://localhost:4000/image/bob2.jpg'},
+    {title:"title 3",
+    description:'description 3',
+    imageUrl:'http://localhost:4000/image/bob3.jpg'},
+    {title:"title 4",
+    description:'description 4',
+    imageUrl:'http://localhost:4000/image/bob4.jpg'},
+    {title:"title 5",
+    description:'description 5',
+    imageUrl:'http://localhost:4000/image/bob5.jpeg'},
+  ]);
+});
+
+app.listen(port, () => {
+  console.log("http://localhost:4000 start");
+});
