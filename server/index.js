ed86ad40bb6f5cff59211b2a01996c98aff605a6
Tatsuya Hirayama <hirayama@feelist.co.jp>
2020-12-09 20:12:13 +0900

    画像の追加とserver側の画像パスを修正

--- server/index.js
+++ server/index.js
@@ -11,19 +11,19 @@ app.get("/", (req, res) => {
   res.json([
     {title:"title 1",
     description:'description 1',
-    imageUrl:'http://localhost:4000/image/bob1.jpg'},
+    imageUrl:'http://localhost:4000/image/img1.jpg'},
     {title:"title 2",
     description:'description 2',
-    imageUrl:'http://localhost:4000/image/bob2.jpg'},
+    imageUrl:'http://localhost:4000/image/img2.jpg'},
     {title:"title 3",
     description:'description 3',
-    imageUrl:'http://localhost:4000/image/bob3.jpg'},
+    imageUrl:'http://localhost:4000/image/img3.jpg'},
     {title:"title 4",
     description:'description 4',
-    imageUrl:'http://localhost:4000/image/bob4.jpg'},
+    imageUrl:'http://localhost:4000/image/img4.jpg'},
     {title:"title 5",
     description:'description 5',
-    imageUrl:'http://localhost:4000/image/bob5.jpeg'},
+    imageUrl:'http://localhost:4000/image/img5.jpg'},
   ]);
 });
 
